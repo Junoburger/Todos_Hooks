2e4803ce4f1b6137b8f9430eaf51bc0dbe90731c
JunoJulius <juno.burger@officebooking.net>
2019-09-22 11:33:53 +0200

    add in resouorces  with hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,34 @@
 import React, { useState } from "react";
+import ResourceList from "./ResourceList";
 
 const App = () => {
-    const [resource, setResource] = useState('posts');
+  const [resource, setResource] = useState("posts");
+  const main = {
+    background: 'rgb(225,225,225)',
+    border: "black 10px dashed",
+    padding: "10px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    minHeight: "100vh"
+  };
+  const button = {
+      background: 'rgb(300,300,300)',
+      border: 'solid grey 1px',
+      fontSize: '21pt',
+      borderRadius: '2px',
+      padding: '10px',
+      cursor: 'pointer'
+  }
+
+
   return (
-    <div>
+    <div style={main}>
       <div>
-        <button onClick={() => setResource('posts')}>
-          Posts
-        </button>
-        <button onClick={() => setResource('todos')}>
-          Todos
-        </button>
+        <button style={button} onClick={() => setResource("posts")}>Posts</button>&nbsp;
+        <button style={button} onClick={() => setResource("todos")}>Todos</button>
       </div>
-      {resource}
+      <ResourceList resource={resource} />
     </div>
   );
 };
